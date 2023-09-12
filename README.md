2939f6cc08849ae5486def4444ae2c90a2643975
Julia Aparecida Moreira <julia.aparecida25@gmail.com>
2023-09-11 22:20:58 -0300

    Finalização da Aplicação

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# memoteca
- 
+# Projeto Memoteca
+Projeto desenvolvido durante dois cursos de Angular 14 da Alura, sendo esses cursos: 
+
+- Angular 14: aplique os conceitos e desenvolva seu primeiro CRUD
+- Angular 14: evoluindo a aplicação
+
+Durante o primeiro curso desenvolvemos uma aplicação do começo ao fim, utilizando o CRUD para definir as suas funcionalidade. A Memoteca é um Single Page Aplication cujo objetivo é armazenar pensamentos que o usuário adiciona, nela é possível inserir, editar e excluir os pensamentos. No segundo curso foi feita algumas melhorias e ajustes onde foram encontradas algumas necessidades, como o campo de busca, favoritar um pensamento e filtrar.
+
+## As tecnologias utilizadas para o desenvolvimento foram:
+• HTML<br>
+• CSS<br>
+• TypeScript<br>
+• Angular<br>
+• Json Server 
+
+## Foto da aplicação 
+![alt text](image/foto-aplicacao.png)
--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
@@ -10,12 +11,54 @@ import { PensamentoService } from '../pensamento.service';
 export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos: Pensamento[] = [];
-  constructor(private service: PensamentoService) { }
+  paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
+  filtro: string = "";
+  favoritos: boolean = false;
+  listaFavoritos: Pensamento[] = [];
+  titulo: string  = 'Meu Mural'
+
+  constructor(private service: PensamentoService, private router: Router) { }
 
   ngOnInit(): void {
-    this.service.listar().subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     })
   }
 
+  carregarMaisPensamentos(){
+    this.service.listar(++this.paginaAtual, this.filtro, this.favoritos).subscribe(listaPensamentos =>{
+      this.listaPensamentos.push(...listaPensamentos);
+      if(!listaPensamentos.length){
+        this.haMaisPensamentos = false;
+      }
+    })
+  }
+
+  pesquisarPensamentos(){
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe(listaPensamentos => {
+      this.listaPensamentos = listaPensamentos;
+    })
+  }
+
+  recarregarComponente(){
+    this.favoritos = false
+    this.paginaAtual = 1
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false
+    this.router.onSameUrlNavigation = 'reload'
+    this.router.navigate([this.router.url])
+  }
+
+  listarFavoritos(){
+    this.titulo = 'Meus Favoritos'
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1
+    this.favoritos = true
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe(listaPensamentosFavoritos => {
+      this.listaPensamentos = listaPensamentosFavoritos
+      this.listaFavoritos = listaPensamentosFavoritos
+    })
+  }
 }
